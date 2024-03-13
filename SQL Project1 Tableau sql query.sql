08dd0b28a42d681ad05ee69f27cfb1e423a84036
Swati Singh Data Analyst <111386815+SwatiDatalance@users.noreply.github.com>
2024-03-13 12:26:12 +0400

    Update SQL Project1 Tableau sql query.sql

--- SQL Project1 Tableau sql query.sql
+++ SQL Project1 Tableau sql query.sql
@@ -69,8 +69,6 @@ order by PercentPopulationInfected desc
 
 
 
--- Queries I originally had, but excluded some because it created too long of video
--- Here only in case you want to check them out
 
 
 -- 1.
@@ -98,8 +96,6 @@ where continent is not null
 order by 1,2
 
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
 
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
@@ -112,8 +108,6 @@ order by 1,2
 
 -- 3.
 
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
